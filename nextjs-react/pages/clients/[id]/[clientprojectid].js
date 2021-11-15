0f3ff0501cc12b7af2f58f2ee8b7cb6d769aef78
Mark <ratrateroo@gmail.com>
2021-11-15 23:43:54 +0800

    added catch all route

--- nextjs-react/pages/clients/[id]/[clientprojectid].js
+++ nextjs-react/pages/clients/[id]/[clientprojectid].js
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 const SelectedClientProjectPage = () => {
+	const router = useRouter();
+
+	console.log(router.pathname);
+	console.log(router.query);
 	return (
 		<div>
 			<h1>Selected Client Project Page</h1>;
